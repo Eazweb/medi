b330c7d3d12ba5f7048125d891b8c42f47f1bbd3
Jaskaran1519 <jaskaran1519@gmail.com>
2024-09-20 14:39:09 +0530

    phone numer changed

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -36,7 +36,7 @@ const Footer = () => {
             <div className="flex">
               <p className="mr-1 text-gray-300">Phone:</p>
               <a
-                href="tel:850-123-5021"
+                href="tel:+919056506403"
                 aria-label="Our phone"
                 title="Our phone"
                 className="transition-colors duration-300 text-gray-300 hover:text-gray-400"
